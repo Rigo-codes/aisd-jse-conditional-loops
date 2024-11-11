3c4bea2167b707afaab26eca291d97e6b8581acf
Rigo-codes <rodrigo.s.m.jr@gmail.com>
2024-11-11 12:32:13 -0800

    learning array shit

--- javascript-loops/index.js
+++ javascript-loops/index.js
@@ -6,13 +6,30 @@
 // }
 
 
-const numbers = [10, 15, 20, 25, 30];
+// const numbers = [10, 15, 20, 25, 30];
 
-console.log("Even and off numbers:");
-for (i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-        console.log(`${numbers[i]} is even`)
+// console.log("Even and off numbers:");
+// for (i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//         console.log(`${numbers[i]} is even`)
+//     } else {
+//         console.log(`${numbers[i]} is odd`)
+//     }
+// }
+
+const scores = [45, 62, 85, 30, 75];
+console.log("Score status:")
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i] >= 60) {
+        console.log(`Score ${scores[i]} is a pass.`)
     } else {
-        console.log(`${numbers[i]} is odd`)
+        console.log(`Score ${scores[i]} is a fail`)
     }
+}
+
+let value = 10;
+console.log("Value Doubling Loop:");
+while (value < 100) {
+    console.log("Value is:" + value);
+    value *= 2;
 }
